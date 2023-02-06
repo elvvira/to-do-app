cd1dca6f929ff89ecf9061689fa6360230722b3f
Daniel Jiménez <elvvira.castellanos@gmail.com>
2023-02-06 14:59:01 +0100

    clase

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,50 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
-import { sayHello } from './demo.js';
+// import { sayHello } from './demo.js';
 
-sayHello();
+const input = document.getElementById('input');
+const form = document.getElementById('form');
+const tasks = document.getElementById('tasks');
+
+const createTask = value => {
+  const timeStamp = Date.now();
+  const fragment = document.createDocumentFragment();
+
+  const taskElement = document.createElement('div');
+  taskElement.classList.add('task');
+  fragment.append(taskElement);
+
+  const taskInput = document.createElement('input');
+  taskInput.classList.add('task__item');
+  taskInput.type = 'checkbox';
+  taskInput.id = timeStamp;
+  taskElement.append(taskInput);
+  const taskLabel = document.createElement('label');
+  taskLabel.classList.add('task__item');
+  taskLabel.textContent = value;
+  taskLabel.htmlFor = timeStamp;
+  taskElement.append(taskLabel);
+  const spanLabel = document.createElement('p');
+  spanLabel.classList.add('task__span');
+  spanLabel.textContent = 'x';
+  taskElement.append(spanLabel);
+
+  tasks.append(fragment);
+};
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+});
+
+input.addEventListener('change', e => {
+  console.dir(e.target.value);
+  createTask(e.target.value);
+  input.value = ' ';
+});
+
+tasks.addEventListener('click', e => {
+  console.dir(e.target);
+  if (e.target.classList.contains('task__span')) {
+    e.target.parentElement.remove();
+  }
+});
